572f1c75feae1b6af24d8dd3f933fa630c7d652e
S.Kosovichev <sergksi@mail.ru>
2023-01-04 23:30:36 +0300

    taks.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
 function compareArrays(arr1, arr2) {
-  
+    // if (arr1.length === arr2.length) {
+    //     for (let i = 0; i < arr1.length; i++) {
+    //         if (arr1[i] !== arr2[i]) {
+    //              return false;
+    //         }
+    //     }
+    // } else {
+    //     return false;
+    //     }
+    // return true;
+return (arr1.length == arr2.length) && arr1.every(function(element, index) {
+    return element === arr2[index]; 
+    });
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+}
+
+function advancedFilter(arr) {
+    let result = arr.filter(element => element > 0).filter(element => element % 3 === 0).map(element => element * 10);
 }
