c0bf463351d181b2fd7e728df51a0f47c0d35c6f
S.Kosovichev <sergksi@mail.ru>
2023-01-05 17:46:25 +0300

    update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,23 +1,16 @@
 function compareArrays(arr1, arr2) {
-    // if (arr1.length === arr2.length) {
-    //     for (let i = 0; i < arr1.length; i++) {
-    //         if (arr1[i] !== arr2[i]) {
-    //              return false;
-    //         }
-    //     }
-    // } else {
-    //     return false;
-    //     }
-    // return true;
-return (arr1.length == arr2.length) && arr1.every(function(element, index) {
+return (arr1.length == arr2.length) && arr1.every((element, index)=> {
     return element === arr2[index]; 
     });
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-
-}
-
-function advancedFilter(arr) {
-    let result = arr.filter(element => element > 0).filter(element => element % 3 === 0).map(element => element * 10);
+    let avrg = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr)=> {
+        acc +=item;
+        if (index === arr.length - 1) {
+          return acc / arr.length;
+        }                                                         
+        return acc;
+      }, 0);
+      return avrg;
 }
